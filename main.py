69b2957bdd2559e0fa1dbbee2f6aeb1e2ff678bb
Алмаз Нуртдинов <akulij@Noutbuk-Almaz.local>
2023-10-31 12:34:04 +0300

    use client config from db to setup process

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import asyncio
-import json
+from sys import argv
 
 from telethon import TelegramClient
-from telethon.hints import Entity
-from telethon.tl.types import Message, Channel, User
+from telethon.tl.types import Channel, User
 from telethon import events
 from telethon.tl.functions.channels import JoinChannelRequest, GetFullChannelRequest
+from pydantic import BaseModel
 
 from async_gpt import GPT
+from modules.db import DB
+from modules.config import config
 
 
+class Config(BaseModel):
+    api_id: int
+    api_hash: str
+    openai_key: str
+    openai_endpoint: str
+    admin_user: str | None
+    session_file: str
+    channels_file: str
+    gpt_prompt: str
 
 
-async def get_admin_user(client: TelegramClient) -> User:
-    return await client.get_entity(ADMIN_USER)
+async def get_admin_user(client: TelegramClient, admin: str | None) -> User | None:
+    if not admin: return None
+    return await client.get_entity(admin)
 
 async def get_channels(client: TelegramClient) -> list[Channel]:
     ch_links: list[str] = []
@@ -32,8 +44,9 @@ async def joined(c: Channel) -> bool:
     if channel: return True
     return False
 
-async def main():
-    admin = await get_admin_user(client)
+async def main(client: TelegramClient, config: Config):
+    gpt = GPT(config.openai_key, api_endpoint=config.openai_endpoint, system_prompt=config.gpt_prompt)
+    admin = await get_admin_user(client, config.admin_user)
     channels = await get_channels(client)
     for channel in channels:
         print(f"checking channel {channel}")
@@ -49,15 +62,47 @@ async def main():
         try:
             comment_text = await gpt.complete(post_text)
         except Exception as e:
-            await client.send_message(admin, f"Unable to generate comment to post {post_link} cause:\n{e}")
+            if admin:
+                await client.send_message(admin, f"Unable to generate comment to post {post_link} cause:\n{e}")
             return
         try:
             await client.send_message(event.chat, comment_text, comment_to=event.message)
         except Exception as e:
-            await client.send_message(admin, f"Unable to LEAVE comment to post {post_link} cause:\n{e}")
+            if admin:
+                await client.send_message(admin, f"Unable to LEAVE comment to post {post_link} cause:\n{e}")
 
-    await client.run_until_disconnected()
+    runner = client.run_until_disconnected()
+    if runner:
+        await runner
+
+async def initalize(client_id: int) -> Config:
+    db = DB(config)
+    cc = await db.get_client_config(client_id)
+    if not cc:
+        raise Exception(f"Not found ClientConfig for {client_id=}")
+    c = Config(
+            api_id=cc.api_id,
+            api_hash=cc.api_hash,
+            openai_key=cc.openai_key,
+            openai_endpoint=cc.openai_endpoint,
+            admin_user=cc.admin_user,
+            session_file=cc.session_file,
+            channels_file=cc.channels_file,
+            gpt_prompt=cc.gpt_prompt
+            )
+
+    return c
+
+def get_client_id() -> int:
+    try:
+        return int(argv[1])
+    except:
+        print(f"Usage: {argv[0]} (int_client_id)")
+        exit(-1)
 
 if __name__ == "__main__":
+    client_id = get_client_id()
+    config = asyncio.run(initalize(client_id))
+    client = TelegramClient(config.session_file, config.api_id, config.api_hash)
     with client:
-        client.loop.run_until_complete(main())
+        client.loop.run_until_complete(main(client, config))
